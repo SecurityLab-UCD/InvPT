78f6db85a0c219c691ee485479735cfaa4900ea1
Chris Gaw <ccgawgonzalo@ucdavis.edu>
2025-01-13 11:28:53 -0800

    Variable Rename Implemented

---
+++ cplus_transforms/example.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int stuff() {
+    int D = 20;
+    int s = D % 12;
+    std::cout << s;
+    return 1;
+}
+
+int main() {
+    int x = 5;
+    int assault = 10;
+    if(x == 10) { // This one here is first
+        x = 6;
+    } else if(x == 5) { // This one is burried beneath the other if statement
+        x = 7;
+    } else {
+        x = 0;
+        assault = 1;
+    }
+    x++;
+    // x+=1;
+    // x--;
+    // x+=2;
+    // x=x+2;
+    stuff(); stuff();
+    return 0;
+}
