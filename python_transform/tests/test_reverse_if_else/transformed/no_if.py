a1413ff18cd2dc86fc28fd57372f3633e097e78b
Zili Wang <zwang400@ucsc.edu>
2025-01-27 10:06:39 -0800

    removed old files under tests/

--- python_transform/tests/test_reverse_if_else/transformed/no_if.py
+++
@@ -1,7 +0,0 @@
-def greet(language):
-    greetings = {'en': 'Hello', 'es': 'Hola', 'fr': 'Bonjour'}
-    return greetings.get(language, 'Hello')
-numbers = [1, 2, 3, 4, 5]
-even_numbers = [num for num in numbers if num % 2 == 0]
-print(even_numbers)
-print(greet('es'))
