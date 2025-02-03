9acdc3262873436114ec3bf383ca6232b7f88650
Zili Wang <zwang400@ucsc.edu>
2025-02-03 22:21:03 +0000

    initial commit for downstream/Clone-detection-CodeNet-Python800

--- python_transform/transform.py
+++ python_transform/transform.py
@@ -16,10 +16,12 @@ from returns.maybe import Maybe, Nothing, Some
 from returns.pointfree import bind
 import tempfile
 import argparse
-from typing import Type
+from typing import Type, Tuple
 from multiprocessing import cpu_count
 from pathos.multiprocessing import ProcessingPool as Pool
-from functools import partial
+from functools import partial, singledispatch
+from pathlib import Path
+import copy
 
 
 TRANSFORMATION_MAP: dict[AugType, Type[ast.NodeTransformer]] = {
@@ -28,6 +30,15 @@ TRANSFORMATION_MAP: dict[AugType, Type[ast.NodeTransformer]] = {
     AugType.ADDASSIGNMENT2EQUALASSIGNMENT: OpAssignment2EqualAssignment,
 }
 
+def convert_source_to_ast_module(source_code: str) -> Maybe[ast.Module]:
+    original_ast_module: Maybe[ast.Module]
+    try:        
+        original_ast_module = Some(ast.parse(source_code))
+    except SyntaxError: 
+        original_ast_module = convert_python2_to_python3(source_code).map(ast.parse)
+    except RecursionError as e:
+        return Nothing
+    return original_ast_module
 
 def convert_python2_to_python3(source_code: str) -> Maybe[str]:
     """convert python 2.x syntax to python 3.x syntax
@@ -55,6 +66,8 @@ def convert_python2_to_python3(source_code: str) -> Maybe[str]:
         # while trying to convert the code from py2 to py3, it will throw an error if the code ifself has syntax error
         # in this case, we skip this transformation
         return Nothing
+    except RecursionError as e:
+        return Nothing
 
     # Read the modified content from the file
     modified_code = temp.read().decode()
@@ -62,13 +75,48 @@ def convert_python2_to_python3(source_code: str) -> Maybe[str]:
     return Some(modified_code)
 
 
-def transform(origional_example: CodeSearchNetExample) -> Maybe[CodeSearchNetExample]:
+@singledispatch
+def transform(origional_example):
+    raise NotImplementedError("Unsupported type!")
+
+@transform.register
+def _(py_file_path: str) -> Maybe[Tuple[str, str, str]]:
+    """
+    Apply three ast.NodeTransformers on the source code, and return three transformed codes.
+    Returns `Nothing` if all transformations fail, otherwise returns a tuple of three.
+    """
+    ast_localVariableRenamer = LocalVariableRenamer()
+    ast_reverseIfElse = ReverseIfElser()
+    ast_opAss2EqualAss = OpAssignment2EqualAssignment()
+
+    with open(py_file_path) as f:
+        lines = f.readlines()
+        source_code = ''.join(lines)
+
+    def apply_transformation(transformer: ast.NodeTransformer) -> Maybe[str]:        
+        original_ast_module: Maybe[ast.Module] = convert_source_to_ast_module(source_code)
+        if original_ast_module == Nothing: return Nothing
+        try:
+            return original_ast_module.map(transformer.visit).map(ast.unparse)
+        except RecursionError:
+            return Nothing
+
+    # Apply all transformations
+    transformed_results: List[Maybe[str]] = [apply_transformation(TRANSFORMATION_MAP[k]()) for k in TRANSFORMATION_MAP.keys()]
+    
+    # If all transformations fail, return Nothing
+    if all(result == Nothing for result in transformed_results): return Nothing
+
+    # If some of results are None (when an error is raised while parsing AST tree), mark it as None
+    return Some(tuple(result.value_or(None) for result in transformed_results))
+
+@transform.register
+def _(origional_example: CodeSearchNetExample) -> Maybe[CodeSearchNetExample]:
     """
     Apply the ast.NodeTransformer class on the source code and return the transformed code
 
     """
-    # parse the source code into AST
-    # print('source code: ', source)
+    # parse the source code into AST 
 
     source = origional_example.code
     transform_type = origional_example.aug_type
@@ -82,17 +130,22 @@ def transform(origional_example: CodeSearchNetExample) -> Maybe[CodeSearchNetExa
     except SyntaxError:
         # Ohterwise, convert to pyhon 3.x syntax from python 2.x
         original_ast_module = convert_python2_to_python3(source).map(ast.parse)
+    except RecursionError as e:
+        return Nothing
 
     def add_transformed_code(transformed_code: str):
         origional_example.transformed = transformed_code
         return origional_example
 
     # ToDo: check if `source` is really modified. If not, return Nothing
-    return (
+    try:
+        return (
         original_ast_module.map(ast_transformer.visit)
         .map(ast.unparse)
         .map(add_transformed_code)
     )
+    except RecursionError as e:
+        return Nothing
 
 
 def load_csn_example(augtype: AugType, json_line: str) -> Maybe[CodeSearchNetExample]:
@@ -123,7 +176,7 @@ def main(
 
     assert os.path.exists(input_file_path) and os.path.isfile(
         input_file_path
-    ), "Invalid input file path"
+    ), "Invalid input file path"    
 
     # read in the jsonl file
     with open(input_file_path, "r") as f:
@@ -136,9 +189,9 @@ def main(
     with open(output_file_path, "w") as f:
         for transformed_csn in transformed_data:
             match transformed_csn:
-                case Some(csn):
+                case Some(csn):                    
                     f.write(json.dumps(asdict(csn)) + "\n")
-                case Nothing:
+                case Nothing:                    
                     pass
 
     print("\nFinished Transformed!\n\n")
@@ -157,13 +210,13 @@ if __name__ == "__main__":
     arg_parser.add_argument(
         "-i",
         "--input_path",
-        help="Path to jsonl file for transformation",
+        help="Path to py/jsonl file for transformation",
         type=str,
     )
     arg_parser.add_argument(
         "-o",
         "--output_path",
-        help="The target jsonl file where the transformed code are located",
+        help="The target py/jsonl file where the transformed code are located",
         type=str,
     )
     arg_parser.add_argument(
@@ -172,7 +225,7 @@ if __name__ == "__main__":
         help="The number of CPU cores to use for parallel processing",
         type=int,
         default=cpu_count(),
-    )
+    )    
 
     args = arg_parser.parse_args()
     main(AugType(args.augtype), args.input_path, args.output_path, args.num_cpus)
