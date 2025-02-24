686b1f6171f74aff2c9e20d6d858d21249a3f3e9
Eric Xu <xydxydxyd1@gmail.com>
2025-02-23 20:07:40 -0800

    add!: return original if failed

--- java_transform/augment.py
+++ java_transform/augment.py
@@ -1,7 +1,9 @@
 from pathlib import Path
 from tqdm import tqdm
+from typing import TypedDict, Union
 import argparse
 import json
+import numpy as np
 import os
 import pandas as pd
 import shutil
@@ -65,51 +67,88 @@ def decompose(original_df, code_dir):
         with open(java_path, "w") as f:
             f.write(entry.code)
 
+def postprocess(original: pd.DataFrame, transformed_path: Path, rule_id: int) -> pd.DataFrame:
+    """Process SPAT output
+
+    Arguments:
+    original -- Unaugmented code. (index: int, code: str, label: Any)
+    transforme_path -- The path to SPAT output java files
+    rule_id -- the ID of the rule for transformation
+
+    Returns: Augmented dataframe (code: str, label: Any, aug_type: str, success:
+    bool, (index) aug_from: int)
+    """
+    augmented = pd.DataFrame(original)
+    augmented['aug_type'] = pd.Series(np.full(augmented.shape[0], id_to_name[rule_id]))
+    augmented['success'] = pd.Series(np.full(augmented.shape[0], False))
+    augmented = augmented.rename({'index': 'aug_from'}, axis='columns')
+    augmented = augmented.set_index('aug_from')
+
+    for file in tqdm(os.listdir(transformed_path)):
+        aug_from = int(file.lstrip('n').rstrip('.java'))
+        with open(transformed_path / file) as f:
+            transformed = f.read()
+        augmented.loc[aug_from, 'code'] = transformed
+        augmented.loc[aug_from, 'success'] = True
+    return augmented
 
 
-def spat(spat_jar, df, rule_ids, lib_path, output_path):
-    """Run SPAT on all entries in the dataframe and append the results to
-    output_path
+def spat(original: pd.DataFrame, spat_jar: Path, rule_ids: list[int],
+         lib_path: Path) -> pd.DataFrame:
+    """Run SPAT on `original`, returning a DataFrame containing
+    augmented entries.
 
-    The original (unaugmented) entries are not written
+    If an entry of `original` cannot be augmented by a given rule, its
+    corresponding entry in the output would contain the original code and
+    success=False
+
+    Arguments:
+    original -- Unaugmented code. (index: int, code: str, label: Any)
+    spat_jar -- Path to the SPAT jarfile
+    rule_ids -- the IDs of the augmentation rule; see README
+    lib_path -- the library used by SPAT
+
+    Returns: Dataframe (index: int, code: str, label: Any, aug_type: str,
+    success: bool, aug_from: int)
     """
     artifact_path = Path('tmp')
     transformed_path = artifact_path / Path('transformed')
+    original_path = artifact_path / Path('original')
     os.mkdir(artifact_path)
-    os.mkdir(artifact_path / 'original')
+    os.mkdir(original_path)
 
-    new_id = int(df["index"].max()) + 1
-    print(new_id)
-    decompose(df, artifact_path / 'original')
+    dfs = []
+    decompose(original, artifact_path / 'original')
     for rule_id in rule_ids:
         print(f'Augmenting dataset with rule {rule_id}...')
         subprocess.run(
-            ["java", "-jar", spat_jar, str(rule_id), artifact_path / 'original',
-                 transformed_path, lib_path],
+            ["java", "-jar", spat_jar, str(rule_id), original_path,
+             transformed_path, lib_path],
             stdout=subprocess.DEVNULL,
             stderr=subprocess.DEVNULL)
-        print('Saving results')
-        for file in tqdm(os.listdir(transformed_path)):
-            code_id = int(file.lstrip('n').rstrip('.java'))
-            with open(transformed_path / file) as f:
-                transformed = f.read()
-            entry = df.loc[df['index'] == code_id].iloc[0].to_dict()
-            entry = {
-                'index': new_id,
-                'label': entry['label'],
-                'code': transformed,
-                'aug_type': id_to_name[rule_id],
-                'aug_from': entry['index']
-            }
-            new_id += 1
-            with open(output_path, 'a') as f:
-                f.write(f'{json.dumps(entry)}\n')
+        dfs.append(postprocess(original, transformed_path, rule_id))
         shutil.rmtree(transformed_path)
     shutil.rmtree(artifact_path)
+
+    original['aug_type'] = pd.Series(np.full(original.shape[0], 'None'))
+    original['success'] = pd.Series(np.full(original.shape[0], True))
+    original['aug_from'] = original['index']
+    index = original['index'].max() + 1
+    for i in range(0, len(dfs)):
+        dfs[i]['index'] = range(index, index + len(dfs[i]))
+        dfs[i] = dfs[i].reset_index()
+        index += len(dfs[i])
+    output = pd.concat([original, *dfs], ignore_index=True)
+
+    return output
+
 
 if __name__ == "__main__":
     args = parser.parse_args()
     original = jsonl_to_df(args.input_path)
     assert set(['label', 'index', 'code']).issubset(original.columns)
     shutil.copyfile(args.input_path, args.output_path)
-    spat(args.spat_jar, original, args.rules, args.spat_lib, args.output_path)
+    df_result = spat(original, args.spat_jar, args.rules, args.spat_lib)
+    print(df_result)
+    with open(args.output_path, "w") as f:
+        f.write(df_result.to_json(orient='records', lines=True, force_ascii=False))
