4e34006b2f9cf7eed2e0f31c7a37e6ac848adc4c
chriscgaw16 <christophercgaw@gmail.com>
2025-02-24 11:49:15 -0800

    feat: integrate ast_util functions to standardize transformation code

--- cplus_transforms/transformations/for_while.py
+++ cplus_transforms/transformations/for_while.py
@@ -2,43 +2,6 @@ from clang.cindex import Index, CursorKind
 from collections import deque
 import sys
 from ast_util import *
-# # Traverse and get all if statements
-# def generate_hidden_name(str):
-#     replaced_char = chr(ord(str[0]) - ord('a') + 1138)
-#     random_name = replaced_char + str[1:]
-#     return random_name
-
-# # 
-# def get_character_offset(file_path, line, column):
-#     """Convert line and column into absolute character offset."""
-#     with open(file_path, "r", encoding="utf-8") as f:
-#         lines = f.readlines()
-    
-#     char_pos = sum(len(lines[i]) for i in range(line - 1))  # - 1 Adjust for zero-index
-#     char_pos += column - 1  # Adjust for zero-based index
-#     return char_pos
-
-# def get_offset(node):
-#     return get_character_offset(node.location.file.name, node.extent.start.line, node.extent.start.column)
-
-# # Function to get start and end character positions of a node
-# def get_node_char_positions(node):
-#     """Returns (start_offset, end_offset) of a node."""
-#     if node.location.file and node.extent.start.file:  # Ensure valid locations
-#         file_path = node.location.file.name
-#         print(node.extent.start)
-#         start_offset = get_character_offset(file_path, node.extent.start.line, node.extent.start.column)
-#         end_offset = get_character_offset(file_path, node.extent.end.line, node.extent.end.column)
-#         return start_offset, end_offset
-#     return [None, None]
-
-# def extract_source_code(node):
-#     """Extract the source code for the node."""
-#     extent = node.extent
-#     with open(extent.start.file.name, 'r') as f:
-#         code = f.read()
-#     start_end = get_node_char_positions(node)
-#     return code[start_end[0]:start_end[1]]
 
 def for_while_reverse(root_node, source_file, source_code, start_end):
     # Collect all nodes that have IF_STMT
--- cplus_transforms/transformations/p2add.py
+++ cplus_transforms/transformations/p2add.py
@@ -1,48 +1,7 @@
 from clang.cindex import Index, CursorKind
 from collections import deque
 import sys
-import random
-
-EXPRESSION_TYPES = [
-    CursorKind.UNEXPOSED_EXPR,
-    CursorKind.BINARY_OPERATOR,
-    CursorKind.UNARY_OPERATOR,
-    CursorKind.CONDITIONAL_OPERATOR,
-    CursorKind.CSTYLE_CAST_EXPR,
-    CursorKind.COMPOUND_LITERAL_EXPR,
-    CursorKind.INIT_LIST_EXPR,
-    CursorKind.ADDR_LABEL_EXPR,
-    CursorKind.OBJC_STRING_LITERAL,
-    CursorKind.OBJC_ENCODE_EXPR,
-    CursorKind.OBJC_SELECTOR_EXPR,
-    CursorKind.OBJC_PROTOCOL_EXPR,
-    CursorKind.OBJC_BRIDGE_CAST_EXPR,
-    CursorKind.PACK_EXPANSION_EXPR,
-    CursorKind.SIZE_OF_PACK_EXPR,
-    CursorKind.PAREN_EXPR
-]
-
-def generate_hidden_name(i, length = -1):
-    generator = random.Random()
-    generator.seed(i + 2025)
-    if length == -1:
-        length = generator.randint(10, 25)
-    name_code = ((11 * i) + 2025) % 32**length
-    random_name = ""
-    for i in range(length):
-        random_name += chr(name_code % 31)
-        name_code //= 32
-    return random_name
-
-def extract_source_code(node):
-    """Extract the source code for the node."""
-    extent = node.extent
-    with open(extent.start.file.name, 'r') as f:
-        lines = f.readlines()
-    start_line, start_col = extent.start.line, extent.start.column
-    end_line, end_col = extent.end.line, extent.end.column
-    code = ''.join(lines[start_line-1:end_line])
-    return code[start_col-1:end_col-1]
+from ast_util import *
 
 def replace_short_add(root_node, source_file, source_code_lines, modifications):
     # Collect all nodes that have function names
@@ -60,20 +19,18 @@ def replace_short_add(root_node, source_file, source_code_lines, modifications):
         if curr_visit.kind == CursorKind.UNARY_OPERATOR:
             if "++" in source_code.strip()[:2]:
                  change_nodes.append(curr_visit)
-            if "++" in source_code.strip()[length-2:] and not (parent.kind in EXPRESSION_TYPES):
+            if "++" in source_code.strip()[length-2:] and not (parent.kind in OUTPUT_USING_EXPRESSION_TYPES):
                  change_nodes.append(curr_visit)
 
     replace_dictionary = {}
 
-    i = 0
     # Replace with names that are same length and present
     for node in change_nodes:
         line_number = node.location.line
         column_number = node.location.column
         structure_name = extract_source_code(node).strip()
         
-        edited = generate_hidden_name(i, len(structure_name))
-        i += 1
+        edited = generate_hidden_name(structure_name)
         replace_dictionary[edited] = "(" + structure_name.strip("+").strip() + "+=1)"
         print(f"Expression {structure_name} will be renamed to {replace_dictionary[edited]}")
 
