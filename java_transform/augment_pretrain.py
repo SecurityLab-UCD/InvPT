a628e5d2470d3b5a943b1fdbbcc278271e7db7c5
Eric Xu <xydxydxyd1@gmail.com>
2025-02-28 16:19:02 -0800

    add: augment_pretrain.py

--- java_transform/augment_pretrain.py
+++ java_transform/augment_pretrain.py
@@ -1,19 +1,40 @@
-import fire
 from pathlib import Path
-from augment import spat, jsonl_to_df, DIR_PATH
+from tempfile import TemporaryDirectory
+from tqdm import tqdm
+from utils import jsonl_to_df, decompose, id_to_name, DIR_PATH
+import numpy as np
+import os
+import json
+import fire
+import pandas as pd
+import subprocess
 
 
-def col_key(col: str) -> float:
-    """Key for sorting the columns"""
-    match col:
-        case "code":
-            return 0
-        case "transformed":
-            return 1
-        case "aug_type":
-            return 2
+def postprocess(
+    original: pd.DataFrame,
+    transformed_path: Path,
+    output_path: Path,
+    rule_id: int,
+):
+    """Processes SPAT output and append to `output_path`.
 
-    return float("inf")
+    Args:
+    original -- the original dataset (code: str)
+    transformed_path -- the path of the files transformed by SPAT
+    output_path -- the path to append the SPAT output to
+    rule_id -- the ID of the SPAT transformation rule
+    """
+    with open(output_path, 'a') as output_file:
+        for file in tqdm(os.listdir(transformed_path), desc="Processing SPAT output"):
+            aug_from = int(file.lstrip("n").rstrip(".java"))
+            with open(transformed_path / file) as transformed_file:
+                transformed = transformed_file.read()
+            entry = {
+                **original.loc[aug_from],
+                "aug_type": id_to_name[rule_id],
+                "transformed": transformed,
+            }
+            output_file.write(f'{json.dumps(entry)}\n')
 
 
 def main(
@@ -22,9 +43,8 @@ def main(
     spat_jar: str = str(DIR_PATH / "SPAT-linux.jar"),
     spat_lib: str = "/usr/lib/jvm/java-18-openjdk-amd64/lib",
     rules: list[int] = [0, 1, 2, 3, 6, 7],
-    include_original: bool = True,
 ):
-    """Augment pretrain CodeSearchNet dataset
+    """Augment pretrain dataset
 
     Args:
     input_path: Path to the input jsonl (code: str)
@@ -33,26 +53,37 @@ def main(
     spat_jar: Path to the SPAT jar file used for augmentation
     spat_lib: Path to the Java library used by SPAT (see SPAT documentation)
     rules: List of SPAT rule IDs.
-    include_original: If True, the augmented dataset contains the original
-    accumulate: If True, the rules are applied accumulatively instead of
-        mapping (i.e. [t1(t2(x)]) instead of [t1(x), t2(x)])
     """
     original = jsonl_to_df(input_path)
-
-    augmented = spat(original, Path(spat_jar), rules, Path(spat_lib), include_original)
-    augmented = augmented.loc[augmented["aug_success_0"]]
-    augmented.rename(
-        {"aug_type_0": "aug_type", "code": "transformed", "aug_from_0": "code"},
-        axis="columns",
-        inplace=True,
+    print("Processing original dataset. This might take a minute...")
+    original["transformed"] = original["code"]
+    original["aug_type"] = pd.Series(
+        np.full(original.shape[0], "None"), index=original.index
     )
-    augmented["code"] = augmented["code"].apply(lambda id: original.loc[id, "code"])
-    augmented.drop(["aug_success_0"], axis="columns", inplace=True)
-
-    augmented = augmented[sorted(augmented.columns, key=col_key)]
     with open(output_path, "w") as f:
-        f.write(augmented.to_json(orient="records", lines=True, force_ascii=False))
-
+        f.write(original.to_json(orient="records", lines=True, force_ascii=False))
+    print("Augmenting original dataset")
+    with TemporaryDirectory() as original_dir:
+        decompose(original, Path(original_dir))
+        for rule in rules:
+            print(f"Augmenting dataset with rule {rule}...")
+            with TemporaryDirectory() as transformed_dir:
+                subprocess.run(
+                    [
+                        "java",
+                        "-jar",
+                        spat_jar,
+                        str(rule),
+                        original_dir,
+                        transformed_dir,
+                        spat_lib,
+                    ],
+                    stdout=subprocess.DEVNULL,
+                    stderr=subprocess.DEVNULL,
+                )
+                postprocess(
+                    original, Path(transformed_dir), Path(output_path), rule
+                )
 
 if __name__ == "__main__":
     fire.Fire(main)
