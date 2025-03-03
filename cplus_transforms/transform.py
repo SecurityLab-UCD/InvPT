63923bb2dc95f6cc64848131d93587a46396bf48
chriscgaw16 <christophercgaw@gmail.com>
2025-03-03 11:39:57 -0800

    Feat: transform.py

---
+++ cplus_transforms/transform.py
@@ -0,0 +1,133 @@
+import os
+from dataclasses import asdict
+from transformations.addassignment import add_assignmenter
+from transformations.local_rename import local_renamer
+from transformations.p2add import replace_short_adder
+from transformations.for_while import for_while_reverser
+from transformations.while_for import while_for_reverser
+from transformations.if_else_transform import if_else_reverser
+from modeling.dataloader import CodeSearchNetExample, AugType
+import ast
+import json
+import subprocess
+from returns.maybe import Maybe, Nothing, Some
+from returns.pointfree import bind
+import tempfile
+import argparse
+from typing import Type
+from multiprocessing import cpu_count
+from pathos.multiprocessing import ProcessingPool as Pool
+from functools import partial
+import clang
+
+
+TRANSFORMATION_MAP: dict[AugType, Type[ast.NodeTransformer]] = {
+    AugType.LOCALVARRENAMING: local_renamer,
+    AugType.REVERSEIFELSE: if_else_reverser,
+    AugType.ADDASSIGNMENT2EQUALASSIGNMENT: add_assignmenter,
+    AugType.PP2ADDASSIGNMENT: replace_short_adder,
+    AugType.WHILE2FOR: while_for_reverser,
+    AugType.FOR2WHILE: for_while_reverser,
+}
+
+def apply_code_transformation(aug_type: AugType, code: str) -> str:
+    """
+    Given an augmentation type and source code, return the transformed code
+    """
+    ast_transformer = TRANSFORMATION_MAP[aug_type]()
+    index = clang.cindex.Index.create()
+    translation_unit = index.parse('example.cpp', args=['-std=c11'], unsaved_files=[('example.cpp', code)], options=0)
+    return ast_transformer(translation_unit.cursor, code)
+
+
+def transform(csn_example: CodeSearchNetExample) -> Maybe[CodeSearchNetExample]:
+    """
+    Apply the ast.NodeTransformer class on the source code and return the transformed code
+    """
+    csn_example.transformed = apply_code_transformation(
+        csn_example.aug_type, csn_example.code
+    )
+    return csn_example
+
+
+def load_csn_example(augtype: AugType, json_line: str) -> Maybe[CodeSearchNetExample]:
+    data = json.loads(json_line)
+    try:
+        csn_example = CodeSearchNetExample(
+            repo=data["repo"],
+            func_name=data["func_name"],
+            language=data["language"],
+            code=data["code"],
+            docstring=data["docstring"],
+            transformed="",
+            aug_type=augtype,
+        )
+    except KeyError:
+        return Nothing
+
+    return Some(csn_example)
+
+
+def main(
+    augtype: AugType,
+    input_file_path: str,
+    output_file_path: str,
+    num_cpus: int,
+):
+    print(f"-------- Selected Transforming Method: {augtype} -------- ")
+
+    assert os.path.exists(input_file_path) and os.path.isfile(
+        input_file_path
+    ), "Invalid input file path"
+
+    # read in the jsonl file
+    with open(input_file_path, "r") as f:
+        lines = f.read().splitlines()
+
+    with Pool(num_cpus) as pool:
+        csn_examples = pool.map(partial(load_csn_example, augtype), lines)
+        transformed_data = pool.map(bind(transform), csn_examples)
+
+    with open(output_file_path, "w") as f:
+        for transformed_csn in transformed_data:
+            match transformed_csn:
+                case Some(csn):
+                    f.write(json.dumps(asdict(csn)) + "\n")
+                case Nothing:
+                    pass
+
+    print("\nFinished Transformed!\n\n")
+
+
+if __name__ == "__main__":
+    arg_parser = argparse.ArgumentParser()
+    # Parsing Arguments
+    arg_parser.add_argument(
+        "-t",
+        "--augtype",
+        help="The id of transformation method",
+        type=str,
+        choices=[k.value for k in TRANSFORMATION_MAP.keys()],
+    )
+    arg_parser.add_argument(
+        "-i",
+        "--input_path",
+        help="Path to jsonl file for transformation",
+        type=str,
+    )
+    arg_parser.add_argument(
+        "-o",
+        "--output_path",
+        help="The target jsonl file where the transformed code are located",
+        type=str,
+    )
+    arg_parser.add_argument(
+        "-n",
+        "--num_cpus",
+        help="The number of CPU cores to use for parallel processing",
+        type=int,
+        default=cpu_count(),
+    )
+
+    args = arg_parser.parse_args()
+    main(AugType(args.augtype), args.input_path, args.output_path, args.num_cpus)
