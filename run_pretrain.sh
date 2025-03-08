30c67c2de1abe1efa183455f66b54fd2114e3144
Yifeng He <heeafen0602@gmail.com>
2025-03-09 02:22:37 +0800

    set run_pretrain.sh to designed setting

--- run_pretrain.sh
+++ run_pretrain.sh
@@ -1,7 +1,7 @@
 # total batch size = 
 python modeling/train_roberta.py \
     --batch_size=256 \
-    --max_steps=100000 \
+    --max_steps=50000 \
     --model_name="microsoft/codebert-base" \
     --dataset_path="data/codesearchnet_jp.jsonl" \
     --run_name="jp_continue_50k_2e-4" \
@@ -9,8 +9,7 @@ python modeling/train_roberta.py \
     --percentage=1 \
     --gradient_accumulation_steps=1 \
     --learning_rate=2e-4 \
-    --continue_from_pretrained \
-    --resume
+    --continue_from_pretrained
  
 
        
