08c193264f02900802b33b80de30c19ea54ad8dc
chriscgaw16 <christophercgaw@gmail.com>
2025-03-09 17:03:23 -0700

    Feat: update env.sh with Libclang path

--- env.sh
+++ env.sh
@@ -1,3 +1,4 @@
 export PIA_HOME=`pwd`
-export JDK_LIB="/usr/lib/jvm/java-11-openjdk-amd64/lib"
 export PYTHONPATH=$PYTHONPATH:`pwd`
+export JDK_LIB="/usr/lib/jvm/java-11-openjdk-amd64/lib"
+export LIBCLANG_PATH=/usr/lib/llvm-15/lib/libclang.so.1
