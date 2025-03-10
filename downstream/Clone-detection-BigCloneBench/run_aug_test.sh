59673c7d2962fd7558bbefe5150413f0cab890f0
Eric Xu <xydxydxyd1@gmail.com>
2025-03-10 16:09:33 -0700

    add: draft pipeline

--- downstream/Clone-detection-BigCloneBench/run_aug_test.sh
+++ downstream/Clone-detection-BigCloneBench/run_aug_test.sh
@@ -1,17 +1,15 @@
-# Run full pipeline (finetune, normal test, augmented test)
+# Run augmented tests
 model_path=$1
 output_dir=$2
 mkdir -p $output_dir
-touch $output_dir/train.log
+touch $output_dir/aug_train.log
 
-cp dataset/original_data.jsonl dataset/data.jsonl
+cp dataset/augmented_data.jsonl dataset/data.jsonl
 python ./code/run.py \
     --output_dir=$output_dir \
     --model_type=roberta \
     --tokenizer_name=microsoft/codebert-base \
     --model_name_or_path=$model_path \
-    --do_train \
-    --do_eval \
     --do_test \
     --train_data_file=./dataset/train.txt \
     --eval_data_file=./dataset/valid.txt \
@@ -23,9 +21,7 @@ python ./code/run.py \
     --learning_rate 5e-5 \
     --max_grad_norm 1.0 \
     --evaluate_during_training \
-    --seed 123456  2>&1 | tee $output_dir/train.log
+    --seed 123456  2>&1 | tee $output_dir/aug_train.log
 
-cp $output_dir/predictions.txt $output_dir/original_predictions.txt
-python evaluator/evaluator.py -a dataset/test.txt -p $output_dir/predictions.txt > $output_dir/test.log
-
-./run_aug_test.sh $1 $2
+cp $output_dir/predictions.txt $output_dir/augmented_predictions.txt
+python evaluator/evaluator.py -a dataset/test.txt -p $output_dir/predictions.txt > $output_dir/aug_test.log
