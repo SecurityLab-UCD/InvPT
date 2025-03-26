107dc04b49e86725b187f878d30e729b24866d66
Yifeng He <heeafen0602@gmail.com>
2025-03-26 14:21:21 +0800

    fix: install clang+llvm to home

--- clang.sh
+++ clang.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-export EMBDING_HOME=`pwd`
 export LLVM=$HOME/clang+llvm
 
-echo 'Installing LLVM.' >&2
+echo 'Installing LLVM in '$LLVM >&2
 # Which LLVM are we using?
 CLANG_LLVM=clang+llvm-14.0.0-x86_64-linux-gnu-ubuntu-18.04
 if [ ! -d $LLVM ]; then
     LLVM_SRC=https://github.com/llvm/llvm-project/releases/download/llvmorg-14.0.0/$CLANG_LLVM.tar.xz
-    cd $EMBDING_HOME 
+    cd $HOME 
     wget $LLVM_SRC 
     tar -xvf $CLANG_LLVM.tar.xz
     rm $CLANG_LLVM.tar.xz 
--- env.sh
+++ env.sh
@@ -1,6 +1,5 @@
 export PIA_HOME=`pwd`
 export PYTHONPATH=$PYTHONPATH:`pwd`
-export PIA_HOME=`pwd`
 export LLVM=$HOME/clang+llvm
 export LIBCLANG_PATH=$LLVM/lib/libclang.so
 export LD_LIBRARY_PATH=$LLVM/lib:$LD_LIBRARY_PATH
