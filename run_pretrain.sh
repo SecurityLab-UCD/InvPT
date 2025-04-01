3fb07674554c1a3042be407b3b7d600f0f142164
Yifeng He <heeafen0602@gmail.com>
2025-04-01 08:01:15 +0800

    add: stage 1 self-contrastive pretraining

--- run_pretrain.sh
+++ run_pretrain.sh
@@ -1,10 +1,24 @@
+export CUDA_VISIBLE_DEVICES=4,5,6,7 
+
+# stage 1
+python modeling/train_stage1.py \
+    --batch_size=256 \
+    --max_steps=20000 \
+    --model_name="microsoft/graphcodebert-base" \
+    --dataset_path="data/raw_csn.jsonl" \
+    --run_name="InvariantBERT_G-stage1" \
+    --seed=0 \
+    --gradient_accumulation_steps=1 \
+    --learning_rate=4e-4 
+
+
 # total batch size = 
 python modeling/train_roberta.py \
     --batch_size=256 \
     --max_steps=50000 \
     --model_name="microsoft/graphcodebert-base" \
     --dataset_path="data/csn_jp.jsonl" \
-    --run_name="GraphCodeBERT_JP" \
+    --run_name="InvariantBERT_G-stage2" \
     --seed=0 \
     --percentage=1 \
     --gradient_accumulation_steps=1 \
