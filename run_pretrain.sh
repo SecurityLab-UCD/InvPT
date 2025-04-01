1fe4dfea8ae7d719a66742455a582a32cc7f3fb7
Yifeng He <heeafen0602@gmail.com>
2025-04-01 08:18:41 +0800

    fix:pretraining run_name

--- run_pretrain.sh
+++ run_pretrain.sh
@@ -7,7 +7,7 @@ python modeling/train_stage1.py \
     --max_steps=20000 \
     --model_name="microsoft/graphcodebert-base" \
     --dataset_path="data/raw_csn.jsonl" \
-    --run_name="InvariantBERT_G-stage1" \
+    --run_name="InvariantBERT_G" \
     --seed=0 \
     --gradient_accumulation_steps=1 \
     --learning_rate=4e-4 
@@ -19,12 +19,11 @@ python modeling/train_stage2.py \
     --max_steps=50000 \
     --model_name="microsoft/graphcodebert-base" \
     --dataset_path="data/csn_jp.jsonl" \
-    --run_name="InvariantBERT_G-stage2" \
+    --run_name="InvariantBERT_G" \
     --seed=0 \
     --percentage=1 \
     --gradient_accumulation_steps=1 \
-    --learning_rate=2e-4 \
-    --continue_from_pretrained
+    --learning_rate=2e-4
  
 
        
