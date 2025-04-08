394f3050f401bf577e873c0cc0207e482802a3b4
Eric Xu <xydxydxyd1@gmail.com>
2025-04-07 17:44:14 -0700

    add: get UJB dataset

--- downstream/Defect-detection/dataset/get_ujb.py
+++ downstream/Defect-detection/dataset/get_ujb.py
@@ -1,6 +1,7 @@
 # Get UJB dataset in test train eval splits.
 
 import fire
+import os
 from datasets import load_dataset
 from dataclasses import dataclass, asdict
 from pathlib import Path
@@ -11,8 +12,9 @@ import json
 # UJB defect detection huggingface path from their GitHub repository
 UJB_PATH = "ZHENGRAN/code_ujb_defectdetection"
 
+
 @dataclass
-class UJBExample():
+class UJBExample:
     """An entry in the UJB dataset"""
     bug_id: str
     task_id: str
@@ -23,26 +25,25 @@ class UJBExample():
     project: str
     prompt_complete: str
 
-    def get_codexglue(self):
-        """Get example in codexglue format"""
-        return {
-            **asdict(self),
-            "func": self.code,
-            "idx": self.task_id,
-            "target": 1 if self.defective else 0
-        }
 
-def main(output_dir: Path):
+def main(output_dir: Path = Path("ujb")):
+    os.makedirs(output_dir, exist_ok=True)
+    idx = 0
     with open(output_dir / Path("train.jsonl"), "w") as f:
-        for entry in load_dataset(UJB_PATH, split='train[:70%]'):
+        for entry in load_dataset(UJB_PATH, split="train"):
             example = UJBExample(**entry)
-            json.dump(example.get_codexglue(), f)
+            json.dump(
+                {
+                    **asdict(example),
+                    "func": example.code,
+                    "idx": idx,
+                    "target": 1 if example.defective else 0,
+                },
+                f,
+            )
             f.write("\n")
-    with open(output_dir / Path("test.jsonl"), "w") as f:
-        for entry in load_dataset(UJB_PATH, split='train[70%:]'):
-            example = UJBExample(**entry)
-            json.dump(example.get_codexglue(), f)
-            f.write("\n")
+            idx += 1
+
 
 if __name__ == "__main__":
     fire.Fire(main)
