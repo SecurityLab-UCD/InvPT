5031bd325c7f334df50a475ac254b95b19caac53
Yifeng He <heeafen0602@gmail.com>
2025-04-17 01:31:34 +0800

    fix: restore sel_contrast training setting

--- run_pretrain.sh
+++ run_pretrain.sh
@@ -1,15 +1,14 @@
 export CUDA_VISIBLE_DEVICES=4,5,6,7 
 export WANDB_PROJECT="PIA"
 
-RUN_NAME="InvariantBERT_G_mix_all_2e-4"
-# total batch size = 
+RUN_NAME="InvariantBERT_G_self_contrast"
 python modeling/pretrain.py \
     --batch_size=256 \
     --max_steps=50000 \
     --model_name="microsoft/graphcodebert-base" \
-    --dataset_path="data/csn.jsonl" \
+    --dataset_path="data/csn_jp.jsonl" \
     --run_name=$RUN_NAME \
     --seed=0 \
     --gradient_accumulation_steps=1 \
-    --learning_rate=2e-4
+    --learning_rate=5e-5
 
