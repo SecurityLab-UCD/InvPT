4e35828231389099b8e3feddea4514345b61391f
Eric Xu <xydxydxyd1@gmail.com>
2025-04-21 17:22:52 -0700

    add: java accumulative augmentation

---
+++ downstream/Code-translation/data/augment.py
@@ -0,0 +1,21 @@
+from fire import Fire
+from java_transform.utils import Program as JavaProgram
+from java_transform import TRANSFORMATION_MAP as JAVA_AUGMAP
+import logging
+logging.basicConfig(level=logging.INFO)
+
+def main(in_path: str, out_path: str):
+    programs: list[JavaProgram] = []
+    logging.info(f"Loading {in_path}")
+    with open(in_path, 'r') as f:
+        for idx, line in enumerate(f):
+            programs.append((idx, line))
+    for aug_type in JAVA_AUGMAP.keys():
+        logging.info(f"Applying {aug_type}")
+        programs = JAVA_AUGMAP[aug_type](programs)
+    with open(out_path, 'w') as f:
+        f.writelines([program[1] for program in programs])
+
+
+if __name__ == "__main__":
+    Fire(main)
