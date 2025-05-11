e325c557dcd6dbe8f509cfc9e6379e093eda4596
Eric Xu <xydxu@ucdavis.edu>
2025-05-11 21:28:21 +0000

    add: start init.sh and run.sh

--- downstream/Clone-detection-BCB-naturalness-attack/init.sh
+++ downstream/Clone-detection-BCB-naturalness-attack/init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # init.sh 
+# Initialization for this task, only need to run once
 
 docker pull zhouyang996/codebert-attack:v1
--- downstream/Clone-detection-BCB-naturalness-attack/run.sh
+++ downstream/Clone-detection-BCB-naturalness-attack/run.sh
@@ -2,8 +2,11 @@
 # run.sh 
 #
 
+# Reset
+docker rm eric-codebert-attack
+
 docker run --name=eric-codebert-attack \
     --gpus all \
     -it \
-    --mount type=bind,src=/media/zyang/codebases,dst=/workspace \
+    --mount type=bind,src=.,dst=/workspace \
     zhouyang996/codebert-attack:v1
