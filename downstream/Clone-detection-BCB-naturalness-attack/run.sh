960d6e669a71bf33823feb81a8c86507a1ed4dfa
Eric Xu <xydxu@ucdavis.edu>
2025-05-15 20:59:05 +0000

    nat-atk: draft run.sh

--- downstream/Clone-detection-BCB-naturalness-attack/run.sh
+++ downstream/Clone-detection-BCB-naturalness-attack/run.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
-# run.sh 
-#
+# uv run bash run.sh 
 
-# Reset
-docker rm eric-codebert-attack
+cd ./CodeXGLUE/Clone-detection-BigCloneBench
 
-docker run --name=eric-codebert-attack \
-    --gpus all \
-    -it \
-    --mount type=bind,src=.,dst=/workspace \
-    zhouyang996/codebert-attack:v1
+cd code
+python run.py \
+    --output_dir=./saved_models \
+    --model_type=roberta \
+    --config_name=microsoft/codebert-base \
+    --model_name_or_path=microsoft/codebert-base \
+    --tokenizer_name=roberta-base \
+    --do_train \
+    --train_data_file=../dataset/train_sampled.txt \
+    --eval_data_file=../dataset/valid_sampled.txt \
+    --test_data_file=../dataset/test_sampled.txt \
+    --epoch 2 \
+    --block_size 512 \
+    --train_batch_size 16 \
+    --eval_batch_size 32 \
+    --learning_rate 5e-5 \
+    --max_grad_norm 1.0 \
+    --evaluate_during_training \
+    --seed 123456 2>&1| tee train.log
+cd ..
