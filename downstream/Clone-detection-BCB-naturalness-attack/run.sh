1fc212928f13c16475e9e8c6644f7a226ebb7f68
Eric Xu <xydxydxyd1@gmail.com>
2025-05-15 20:12:56 -0700

    nat-atk: inference stage

--- downstream/Clone-detection-BCB-naturalness-attack/run.sh
+++ downstream/Clone-detection-BCB-naturalness-attack/run.sh
@@ -9,8 +9,7 @@ mkdir -p $outdir
 cd ./CodeXGLUE/Clone-detection-BigCloneBench
 
 cd code
-echo "Running finetuning here:"
-ls
+echo "Finetuning"
 python run.py \
     --output_dir="$outdir/saved_models/" \
     --model_type=roberta \
@@ -29,4 +28,26 @@ python run.py \
     --max_grad_norm 1.0 \
     --evaluate_during_training \
     --seed 123456 2>&1| tee "$outdir/train.log"
+cd ..
+
+echo "Inference"
+cd code
+python run.py \
+    --output_dir="$outdir/saved_models" \
+    --model_type=roberta \
+    --config_name=microsoft/codebert-base \
+    --model_name_or_path=microsoft/codebert-base \
+    --tokenizer_name=roberta-base \
+    --do_test \
+    --train_data_file=../dataset/train_sampled.txt \
+    --eval_data_file=../dataset/valid_sampled.txt \
+    --test_data_file=../dataset/test_sampled.txt \
+    --epoch 2 \
+    --block_size 512 \
+    --train_batch_size 16 \
+    --eval_batch_size 32 \
+    --learning_rate 5e-5 \
+    --max_grad_norm 1.0 \
+    --evaluate_during_training \
+    --seed 123456 2>&1| tee "$outdir/test.log"
 cd ..
