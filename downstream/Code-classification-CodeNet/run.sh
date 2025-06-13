ffd135ece050e48e398585e9f814300d882489fc
chriscgaw16 <christophercgaw@gmail.com>
2025-06-13 16:18:20 -0700

    Add: Updated run script

--- downstream/Code-classification-CodeNet/run.sh
+++ downstream/Code-classification-CodeNet/run.sh
@@ -5,36 +5,21 @@ subset=$3
 output_dir=$save_path/$subset
 
 mkdir -p $output_dir
-touch $output_dir/train.log
+touch $output_dir/test_train.log
 
 python ./code/run.py \
     --output_dir=$output_dir \
-    --model_type=roberta \
+    --tokenizer_name=microsoft/codebert-base \
     --model_name_or_path=$model_path \
-    --tokenizer_name=roberta-base \
     --do_train \
     --do_test \
     --train_data_file=./dataset/$subset/train.jsonl \
     --eval_data_file=./dataset/$subset/valid.jsonl \
     --test_data_file=./dataset/$subset/test.jsonl \
-    --epoch 2 \
-    --block_size 400 \
-    --train_batch_size 8 \
-    --eval_batch_size 16 \
+    --num_train_epochs 10 \
+    --block_size 512 \
+    --train_batch_size 32 \
+    --eval_batch_size 64 \
     --learning_rate 2e-5 \
     --max_grad_norm 1.0 \
-    --evaluate_during_training \
-    --seed 123456 2>&1| tee $output_dir/train.log
-
-# test
-python evaluator/extract_answers.py \
-    -c dataset/$subset/test.jsonl \
-    -o $output_dir/answer.jsonl
-
-python evaluator/evaluator.py \
-    -a $output_dir/answer.jsonl \
-    -p $output_dir/predictions.jsonl > $output_dir/test.log
-
-
-echo "Running evaluation for augmented test set..."
-./run_aug_test.sh $model_path $save_path $subset
+    --seed 123456  2>&1 | tee $output_dir/test_train.log
--- downstream/Code-classification-CodeNet/run_aug_test.sh
+++ downstream/Code-classification-CodeNet/run_aug_test.sh
@@ -5,34 +5,21 @@ subset=$3
 output_dir=$save_path/$subset
 
 mkdir -p $output_dir
-touch $output_dir/aug_train.log
+touch $output_dir/test_train.log
 
 python ./code/run.py \
     --output_dir=$output_dir \
-    --model_type=roberta \
+    --tokenizer_name=microsoft/codebert-base \
     --model_name_or_path=$model_path \
-    --tokenizer_name=roberta-base \
+    --do_train \
     --do_test \
     --train_data_file=./dataset/$subset/train.jsonl \
     --eval_data_file=./dataset/$subset/valid.jsonl \
     --test_data_file=./dataset/$subset/aug_test.jsonl \
-    --test_predictions_file=aug_predictions.jsonl \
-    --epoch 2 \
-    --block_size 400 \
+    --num_train_epochs 5 \
+    --block_size 256 \
+    --train_batch_size 8 \
     --eval_batch_size 16 \
     --learning_rate 2e-5 \
     --max_grad_norm 1.0 \
-    --seed 123456 2>&1| tee $output_dir/aug_train.log
-
-echo "Extracting answers..."
-python evaluator/extract_answers.py \
-    -c dataset/$subset/test.jsonl \
-    -o $output_dir/answer.jsonl
-
-
-echo "Evaluating test predictions..."
-python evaluator/evaluator.py \
-    -a $output_dir/answer.jsonl \
-    -p $output_dir/aug_predictions.jsonl > $output_dir/aug_test.log
-
-echo "Done"
+    --seed 123456  2>&1 | tee $output_dir/test_train.log
