bf79be9d2dd8298827e2d43dd0101b73af785076
Yifeng He <heeafen0602@gmail.com>
2025-06-17 08:16:40 +0800

    downstream(CodeNetCls): run.sh also run aug

--- downstream/Code-classification-CodeNet/run.sh
+++ downstream/Code-classification-CodeNet/run.sh
@@ -7,6 +7,7 @@ output_dir=$save_path/$subset
 mkdir -p $output_dir
 touch $output_dir/test_train.log
 
+echo "Running fine-tuning for subset: $subset"
 python ./code/run.py \
     --output_dir=$output_dir \
     --tokenizer_name=microsoft/codebert-base \
@@ -23,3 +24,7 @@ python ./code/run.py \
     --learning_rate 2e-5 \
     --max_grad_norm 1.0 \
     --seed 123456  2>&1 | tee $output_dir/test_train.log
+
+
+echo "Running evaluation for augmented test set..."
+./run_aug_test.sh $model_path $save_path $subset
