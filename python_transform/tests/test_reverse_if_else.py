de2a329a13061a69c492b70bbd80b79cc837f8f3
Yifeng He <yfhe@ucdavis.edu>
2026-02-02 07:26:11 +0000

    fix test

--- python_transform/tests/test_reverse_if_else.py
+++ python_transform/tests/test_reverse_if_else.py
@@ -1,12 +1,34 @@
-import sys
+import ast
+import unittest
 
-sys.path.append("..")
+import ast
+import unittest
 
-from src import ReverseIfElser
-from tests.base_tranform_test import BaseCodeTransformTest
+from python_transform.src import ReverseIfElser
 
 
-class TestReverseIfElse(BaseCodeTransformTest):
+class BaseCodeTransformTest(unittest.TestCase):
+    def get_transformed_code(self, code: str, code_transform_class) -> str:
+        """
+        Return the transformed code snippet based on a specific code transform rule
+        """
+        tree = ast.parse(code)
+        transformer = code_transform_class()
+        transformed_tree = transformer.visit(tree)
+        return ast.unparse(transformed_tree)
+
+    def assert_code_equal(self, code1: str, code2: str):
+        """
+        Compares two codes in AST format.
+
+        Helper Functons from the AST module
+        ast.parse(): Parse the source code into AST node
+        ast.dump(): Return a formatted dump of the tree in node.
+        """
+        tree1 = ast.parse(code1)
+        tree2 = ast.parse(code2)
+        self.assertEqual(ast.dump(tree1), ast.dump(tree2))
+
     # TESTS
     def test_no_if_else(self):
         source_code = """
